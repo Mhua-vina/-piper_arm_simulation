67d95cc09330e7868eb8c82804798d09fac8aa76
Mhua-wina <2742141384@qq.com>
2026-04-01 12:03:32 +0800

    Initial commit: PiPER arm SITL and Sim-to-Real completely working

--- point-assets/Scene-Diffuser/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lemo_pose import LEMOPose
-from .multidex_shadowhand_ur import MultiDexShadowHandUR
-from .lemo_motion import LEMOMotion
-from .scannet_path import ScanNetPath
--- point-assets/Scene-Diffuser/models/planner/planner.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class Planner():
-
-    def __init__(self) -> None:
-        pass
-
-    def gradient(self) -> torch.Tensor:
-        pass
